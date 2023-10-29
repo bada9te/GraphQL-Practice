62f5fb3e8cdce625d716d1989c28a39795dfed84
bada9te <bogdantelep@gmaail.com>
2023-10-29 12:21:03 +0200

    adding new resolvers and def types for movies and users

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,11 +1,31 @@
-const {UserList} = require('../Data');
+const {UserList, MovieList} = require('../Data');
+
 
 module.exports = {
 
     Query: {
-        users() {
+        // user resolvers
+        users: () => {
             return UserList;
-        }
-    }
+        },
+        user: (_, args) => {
+            const id = args.id;
+            return UserList.find(item => item.id === Number(id));
+        },
 
-}
+        // movie resolvers
+        movies: () => {
+            return MovieList;
+        },
+        movie: (_, args) => {
+            const name = args.name;
+            return MovieList.find(item => item.name === name);
+        }
+    },
+
+    User: {
+        favoriteMovies: () => {
+            return MovieList.filter(item => item.yearOfPublication >= 2000 && item.yearOfPublication <= 2007);
+        }
+    },
+};
